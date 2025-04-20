bb10f7741cbdb99c76b61b8815f397211a8e0487
Mike Theodorou <miketheocodes@gmail.com>
2025-04-20 13:58:30 -0600

    feat: adds report generating

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-from stats import count_words, count_characters
+from stats import generate_report
 
-def get_book_text(file_path):
-    with open(file_path) as f:
-        return f.read();
-    
 def main():
-    contents = get_book_text('./books/frankenstein.txt')
-    num_words = count_words(contents)
-    num_characters = count_characters(contents)
-    print(num_characters)
-    print(f"{num_words} words found in the document")
+    print(generate_report('books/frankenstein.txt'))
 
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,41 @@
 from collections import Counter
 
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read();
+
 def count_words(text):
     return len(text.split())
 
 def count_characters(text):
     return Counter(text.lower());
+
+def sort_characters(dict):
+    dicts = []
+    for key in dict:
+        print(key)
+        item = {}
+        item[key] = dict[key]
+        dicts.append(item)
+    dicts.sort(key=lambda d: list(d.values())[0], reverse=True)
+    return dicts
+
+def generate_report(file_path):
+    contents = get_book_text(file_path)
+    num_words = count_words(contents)
+    characters = count_characters(contents)
+    character_dicts = sort_characters(characters)
+
+    output = f'''
+============ BOOKBOT ============
+Analyzing book found at {file_path}...
+----------- Word Count ----------
+Found {num_words} total words
+--------- Character Count -------
+'''
+    for char_dict in character_dicts:
+      for character, count in char_dict.items():
+          if character.isalpha():
+              output += f"{character}: {count}\n"
+    return output;
+    
