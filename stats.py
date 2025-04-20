badc51ae36e668316a8120887efed529e6f31a11
Mike Theodorou <miketheocodes@gmail.com>
2025-04-19 21:26:35 -0600

    refactor: prefers counter class over manual implementation

--- stats.py
+++ stats.py
@@ -1,13 +1,7 @@
+from collections import Counter
+
 def count_words(text):
     return len(text.split())
 
 def count_characters(text):
-    char_map = {}
-
-    for character in text:
-        char = character.lower()
-        if(char in char_map):
-            char_map[char] += 1
-        else:
-            char_map[char] = 1
-    return char_map
+    return Counter(text.lower());
